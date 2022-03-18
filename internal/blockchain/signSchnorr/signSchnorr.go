5f6f3c24bdd9a166b1ee94c75f4fa33f616e2756
Adrien Parrochia <adrien.parrochia@adevinta.com>
2022-03-18 22:25:27 +0100

    [api] add SignSchnorr

--- internal/blockchain/signSchnorr/signSchnorr.go
+++ internal/blockchain/signSchnorr/signSchnorr.go
@@ -1,81 +1,20 @@
-// Copyright (c) 2014-2016 The btcsuite developers
-// Use of this source code is governed by an ISC
-// license that can be found in the LICENSE file.
-
-package txscript_test
+package signschnorr
 
 import (
 	"bytes"
 	"crypto/elliptic"
 	"crypto/hmac"
 	"crypto/sha256"
-
-	"fmt"
 	"hash"
 	"math/big"
-	"os"
-	"testing"
 
-	"github.com/LuisAcerv/btchdwallet/crypt"
-	"github.com/brianium/mnemonic"
 	"github.com/gcash/bchd/bchec"
-	"github.com/stretchr/testify/assert"
-	"github.com/wemeetagain/go-hdwallet"
 )
-
-func TestMain(m *testing.M) {
-	exitVal := m.Run()
-	os.Exit(exitVal)
-}
-
-// This example demonstrates creating a script which pays to a bitcoin address.
-// It also prints the created script hex and uses the DisasmString function to
-// display the disassembled script.
-func Test_Example_(t *testing.T) {
-	var (
-		sign    *Signature
-		pubkey  *bchec.PublicKey
-		privKey *bchec.PrivateKey
-		hash    []byte
-	)
-
-	// @https://github.com/gcash/bchd/blob/87534217bfc8d2c73461f025b5330f145aed9e86/bchec/signature_test.go
-	t.Run("example SignSchnorr", func(t *testing.T) {
-		seed := crypt.CreateHash()
-
-		mnemonic, err := mnemonic.New([]byte(seed), mnemonic.English)
-		assert.NoError(t, err)
-
-		// Create a master private key
-		masterPrv := hdwallet.MasterKey([]byte(mnemonic.Sentence()))
-		privKey, pubkey = bchec.PrivKeyFromBytes(bchec.S256(), masterPrv.Key)
-
-		message := "Satoshi Nakamoto"
-		h := sha256.Sum256([]byte(message))
-		hash = h[:]
-
-		//sign, err = privKey.SignSchnorr(hash)
-		sign, err = signSchnorr(privKey, hash)
-		assert.NoError(t, err)
-		fmt.Println(fmt.Sprintf("**************************** SignSchnorr %+v", sign))
-	})
-
-	t.Run("example SignSchnorr verify", func(t *testing.T) {
-		sig, err := bchec.ParseSchnorrSignature(sign.Serialize())
-		assert.NoError(t, err)
-		valid := sig.Verify(hash, pubkey)
-		assert.True(t, valid)
-	})
-}
-
-// SignatureType enumerates the type of signature. Either ECDSA or Schnorr
-type SignatureType uint8
 
 // Signature is a type representing either an ecdsa or schnorr signature.
 type Signature struct {
-	R       *big.Int
-	S       *big.Int
-	sigType SignatureType
+	R *big.Int
+	S *big.Int
 }
 
 var (
@@ -88,14 +27,14 @@ var (
 )
 
 // signSchnorr signs the hash using the schnorr signature algorithm.
-func signSchnorr(privateKey *bchec.PrivateKey, hash []byte) (*Signature, error) {
-	// The rfc6979 nonce derivation function accepts additional entropy.
-	// We are using the same entropy that is used by bitcoin-abc so our test
-	// vectors will be compatible. This byte string is chosen to avoid collisions
-	// with ECDSA which would render the signature insecure.
-	//
-	// See https://github.com/bitcoincashorg/bitcoincash.org/blob/master/spec/2019-05-15-schnorr.md#recommended-practices-for-secure-signature-generation
-	additionalData := []byte{'S', 'c', 'h', 'n', 'o', 'r', 'r', '+', 'S', 'H', 'A', '2', '5', '6', ' ', ' '}
+// The rfc6979 nonce derivation function accepts additional entropy.
+// We are using the same entropy that is used by bitcoin-abc so our test
+// vectors will be compatible. This byte string is chosen to avoid collisions
+// with ECDSA which would render the signature insecure.
+//
+// See https://github.com/bitcoincashorg/bitcoincash.org/blob/master/spec/2019-05-15-schnorr.md#recommended-practices-for-secure-signature-generation
+func SignSchnorr(privateKey *bchec.PrivateKey, hash []byte) (*Signature, error) {
+	additionalData := []byte{'S', 'c', 'h', 'n', 'o', 'r', 'r', '+', 'S', 'H', 'A', '2', '5', '6', ' ', '+'}
 	k := nonceRFC6979(privateKey.D, hash, additionalData)
 	// Compute point R = k * G
 	rx, ry := privateKey.Curve.ScalarBaseMult(k.Bytes())
@@ -120,6 +59,14 @@ func signSchnorr(privateKey *bchec.PrivateKey, hash []byte) (*Signature, error) 
 		R: rx,
 		S: s,
 	}, nil
+}
+
+func ParseSchnorrSig(sign *Signature, hash []byte, pubkey *bchec.PublicKey) bool {
+	sig, err := bchec.ParseSchnorrSignature(sign.Serialize())
+	if err != nil {
+		return false
+	}
+	return sig.Verify(hash, pubkey)
 }
 
 // nonceRFC6979 generates an ECDSA nonce (`k`) deterministically according to RFC 6979.
